923d66294bc8c21a3aeb6c184ba3ac21ab88cab5
matheus.jmaia <matheus.jmaia@10.135.22.41>
2017-05-24 18:12:11 -0300

    Lista 4 ate o exe14

--- src/lista4/EXEC13.java
+++ src/lista4/EXEC13.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package lista4;
+
+import java.util.Scanner;
 
 /**
  *
  * @author matheus.jmaia
  */
 public class EXEC13 {
+    
+    static String divisores (int n){
+        String divisor = "";
+        for(int i = 1; i <= 100;i++){
+            if(n%i==0){
+                divisor = divisor + i + " ";
+            }
+        }
+        return divisor;
+    }
+ 
+    public static void main(String[] args) {
+        Scanner console = new Scanner (System.in);
+        System.out.println("Insira N:");
+        int n = console.nextInt();
+        String r = divisores(n);
+        System.out.println(n+" e divisor de "+r);
+        
+        
+    }
     
 }
